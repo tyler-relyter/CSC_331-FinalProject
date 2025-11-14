9327ce8536232caeb29101659fa5a366ee8d99c6
Gregory McNutt <amcnutt1996@gmail.com>
2025-11-13 22:00:12 -0500

    finalized idle and walk animations for up down left and right. Changed movement mechanics to use a Vector2d called position to move the player

--- core/src/main/java/com/group7/Player.java
+++ core/src/main/java/com/group7/Player.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -13,12 +14,12 @@ import com.badlogic.gdx.utils.Array;
 
 public class Player {
     //all the stuff the player can have as properties.
-    private float width;
-    private float height;
+    private final float width;
+    private final float height;
     private int health;
+    private final Vector2 position;
     private final Vector2 velocity;
-    public float x;
-    public float y;
+    private final float speed;
 
     // idle animation images (textures)
     private Animation<TextureRegion> idleDownAnimation;
@@ -39,21 +40,31 @@ public class Player {
     //animation timing and direction of player
     private float stateTime;
     private String direction;
+    private boolean isWalking;
     private boolean isAttacking;
 
     // animation texture arrays
-    private Array<Texture> idleAnimationTextures;
+    private final Array<Texture> idleAnimationTextures;
+    private final Array<Texture> walkAnimationTextures;
+    private final Array<Texture> attackAnimationTextures;
 
     public Player(float x, float y){
         // sets all the initial values when you create the player object
-        this.x = x;
-        this.y = y;
-        this.velocity = new Vector2();// this is for movement in libGDX
-        this.stateTime = 0f;
+        this.position = new Vector2(x,y);
+        this.velocity = new Vector2(0, 0); // this is for movement in libGDX to set the rate of player movement.
+        this.speed = 100f;
+        this.stateTime = 0f; //this is the amount of time an animation has been playing.
         this.direction = "down";
+        this.isWalking = false;
         this.isAttacking = false;
-        this.idleAnimationTextures = new Array<>();
-        loadAnimations();
+        //Set player size
+        this.width = 15;
+        this.height = 15;
+        //animation vars
+        this.idleAnimationTextures = new Array<>(); //this holds every created texture for the animations.
+        this.walkAnimationTextures = new Array<>();
+        this.attackAnimationTextures = new Array<>();
+        loadAnimations(); //this loads all player animations into memory.
     }
 
     private void loadAnimations() {
@@ -65,31 +76,79 @@ public class Player {
             idleAnimationTextures.add(frameTexture); //add the textures to the animation array
             idleDownFrames.add(new TextureRegion(frameTexture)); // we add to the global TextureRegion array so we can create and call the animation whenever
         }
-
         // create the idle animation with the frames in the idleDownFrames array of textureRegion objects
         idleDownAnimation = new Animation<>(0.1f, idleDownFrames, Animation.PlayMode.LOOP);
 
-
+        //Idle facing up
         Array<TextureRegion> idleUpFrames = new Array<>();
         for (int i = 0; i<=5; i++){
             Texture frameTexture = new Texture(Gdx.files.internal("Characters/IdleUp/idle_up_0"+i+".png"));
             idleAnimationTextures.add(frameTexture);
-
+            idleUpFrames.add(new TextureRegion((frameTexture)));
         }
+        idleUpAnimation = new Animation<>(0.1f, idleUpFrames, Animation.PlayMode.LOOP);
 
+        //Idle facing left
+        Array<TextureRegion> idleLeftFrames = new Array<>();
+        for (int i = 0; i<=5; i++){
+            Texture frameTexture = new Texture(Gdx.files.internal("Characters/IdleLeft/idle_left_0"+i+".png"));
+            idleAnimationTextures.add(frameTexture);
+            idleLeftFrames.add(new TextureRegion(frameTexture));
+        }
+        idleLeftAnimation = new Animation<>(0.1f, idleLeftFrames, Animation.PlayMode.LOOP);
 
+        //idle facing right
+        Array<TextureRegion> idleRightFrames = new Array<>();
+        for (int i = 0; i<=5; i++){
+            Texture frameTexture = new Texture(Gdx.files.internal("Characters/IdleRight/idle_right_0"+i+".png"));
+            idleAnimationTextures.add(frameTexture);
+            idleRightFrames.add(new TextureRegion(frameTexture));
+        }
+        idleRightAnimation = new Animation<>(0.1f, idleRightFrames, Animation.PlayMode.LOOP);
 
+        //walk downward animations
+        Array<TextureRegion> walkDownFrames = new Array<>();
+        for (int i = 0; i<=5; i++){
+            Texture frameTexture = new Texture(Gdx.files.internal("Characters/WalkDown/walk_down_0"+i+".png"));
+            walkAnimationTextures.add(frameTexture);
+            walkDownFrames.add(new TextureRegion(frameTexture));
+        }
+        walkDownAnimation = new Animation<>(0.1f, walkDownFrames, Animation.PlayMode.LOOP);
 
-        //Set player size
-        this.width = 10;
-        this.height = 10;
+        //walk upward animations
+        Array<TextureRegion> walkUpFrames = new Array<>();
+        for (int i = 0; i<=5; i++){
+            Texture frameTexture = new Texture(Gdx.files.internal("Characters/WalkUp/walk_up_0"+i+".png"));
+            walkAnimationTextures.add(frameTexture);
+            walkUpFrames.add(new TextureRegion(frameTexture));
+        }
+        walkUpAnimation = new Animation<>(0.1f, walkUpFrames, Animation.PlayMode.LOOP);
+
+        //walk left animation
+        Array<TextureRegion> walkLeftFrames = new Array<>();
+        for (int i = 0; i<=5; i++){
+            Texture frameTexture = new Texture(Gdx.files.internal("Characters/WalkLeft/walk_left_0"+i+".png"));
+            walkAnimationTextures.add(frameTexture);
+            walkLeftFrames.add(new TextureRegion(frameTexture));
+        }
+        walkLeftAnimation = new Animation<>(0.1f, walkLeftFrames, Animation.PlayMode.LOOP);
+
+        // walk right animation
+        Array<TextureRegion> walkRightFrames = new Array<>();
+        for (int i = 0; i<=5; i++){
+            Texture frameTexture = new Texture(Gdx.files.internal("Characters/WalkRight/walk_right_0"+i+".png"));
+            walkAnimationTextures.add(frameTexture);
+            walkRightFrames.add(new TextureRegion(frameTexture));
+        }
+        walkRightAnimation = new Animation<>(0.1f, walkRightFrames, Animation.PlayMode.LOOP);
+
     }
 
     public void draw(SpriteBatch spriteBatch){
         Animation<TextureRegion> currentAnimation = idleDownAnimation; // default animation when game loads.
 
         // decide which animation to use
-        if (velocity.isZero()) { // if the character is not moving:
+        if (!isWalking) { // if the character is not moving:
             if (direction.equals("down")){
                 currentAnimation = idleDownAnimation;
             }
@@ -102,60 +161,99 @@ public class Player {
             if (direction.equals("right")){
                 currentAnimation = idleRightAnimation;
             }
-
-        } else { //if the character is moving:
-            currentAnimation = walkDownAnimation;
+        } else {
+            if (direction.equals("down")){
+                currentAnimation = walkDownAnimation;
+            }
+            if (direction.equals("up")) {
+                currentAnimation = walkUpAnimation;
+            }
+            if (direction.equals("left")){
+                currentAnimation = walkLeftAnimation;
+            }
+            if (direction.equals("right")){
+                currentAnimation = walkRightAnimation;
+            }
         }
-
 
 
         //grab the key frame from the active animation to put on the screen
         TextureRegion currentFrame = currentAnimation.getKeyFrame(stateTime, true);
 
         //draw the frame to the screen when draw method is called.
-        spriteBatch.draw(currentFrame, x, y, width, height);
+        spriteBatch.draw(currentFrame, this.position.x, this.position.y, width, height);
     }
 
     public void update(float delta, float worldWidth, float worldHeight){
         handleInput();
         // updates position
-        x+= velocity.x * delta; // multiply it by delta so it doesn't fuck up with different fps
-        y+= velocity.y * delta;
+
+        this.position.add(this.velocity.x * delta, this.velocity.y * delta); //adds the velocity to the players position.
 
         // keep player within world bounds
-        x = MathUtils.clamp(x, 0, worldWidth - width);
-        y = MathUtils.clamp(y, 0, worldHeight - height);
+        this.position.x = MathUtils.clamp(this.position.x, 0, worldWidth - width);
+        this.position.y = MathUtils.clamp(this.position.y, 0, worldHeight - height);
 
-        stateTime += delta;
+        stateTime += delta; //this adds delta time(game time) to the animation time.
+        System.out.println(this.isWalking);
         //because this is called in the main render, it continuously updates the char position (when you move him)
     }
 
     private void handleInput(){
-        velocity.set(0, 0); //this sets the initial velocity to nothing until you press a key
+        this.velocity.set(0, 0); //this sets the initial velocity to nothing until you press a key
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            this.isWalking = true;
             this.direction = "down";
+            this.velocity.y -= speed; // moves character down
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+        else if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+            this.isWalking = true;
             this.direction = "up";
+            this.velocity.y += speed; // moves character up
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            this.isWalking = true;
             this.direction = "left";
+            this.velocity.x -= speed; //moves character left
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            this.isWalking = true;
             this.direction = "right";
+            this.velocity.x += speed; //moves character right
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+        else if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+            this.isWalking = false;
+            this.isAttacking = true;
             //implement attack animation based on direction facing.
         }
+        else {
+            this.isWalking = false;
+            this.isAttacking = false;
+            this.velocity.x = 0;
+            this.velocity.y = 0;
+        }
+//        if (!Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+//            isWalking = false;
+//            this.isAttacking = false;
+//            //basically, this and the above space bar implementation say that if you hold the space bar the attack animation will play
+//            //when you release it, it will stop playing and go back to idle.
+//        }
     }
 
 
     public float getBottomY(){
-        return y; // the bottom of the player sprite
+        return this.position.y; // the bottom of the player sprite
     }
 
     public Rectangle getBounds(){
-        return new Rectangle(x,y,width,height); // the rectangle of the player
+        return new Rectangle(this.position.x,this.position.y,width,height); // the rectangle of the player
+    }
+
+    public float getPositionX(){
+        return this.position.x;
+    }
+    public float getPositionY(){
+        return this.position.y;
     }
 
     public void dispose(){
