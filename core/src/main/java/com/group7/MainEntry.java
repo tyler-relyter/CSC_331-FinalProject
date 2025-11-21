9678c263f98e35919a44a9074f2a1df9494aff94
Gregory McNutt <amcnutt1996@gmail.com>
2025-11-21 10:52:43 -0500

    cleaned up white space, added comments. Added disposal for rest of player textures.

--- core/src/main/java/com/group7/MainEntry.java
+++ core/src/main/java/com/group7/MainEntry.java
@@ -63,11 +63,14 @@ public class MainEntry extends Game {
         player = new Player(worldWidth / 2f, worldHeight / 2f);
         player.setMap(gameMap); // attach map to player so collisions work
 
+        //creates a new basic enemy and sets the position with the map.
         enemy = new BasicEnemy(worldWidth / 2f + 20f, worldHeight / 2f, player);
         enemy.setMap(gameMap);
 
+        // add the enemy to the array of entities being loaded into the game.
         entities.add(enemy);
 
+        // Logic to check the damage between the player and enemy.
         gameDamageLogic = new DamageLogic(player, enemy);
     }
 
@@ -75,6 +78,7 @@ public class MainEntry extends Game {
     public void render(){
         float delta = Gdx.graphics.getDeltaTime(); // compute elapsed time since last frame
 
+        // Makes the game logic check every frame that is rendered.
         gameDamageLogic.update();
 
         for (GameEntity entity : entities) {
@@ -117,6 +121,7 @@ public class MainEntry extends Game {
         float w = player.playerAttackBounds.width;
         float h = player.playerAttackBounds.height;
 
+        // The rectangle that is in front of the player
         tempRect.rect(x, y, w, h);
         tempRect.end();
 
